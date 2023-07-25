23918558e22e076da6ff00ce89e1a13c6cafdc7b
Lucas Lopes <125325077+Luckasklopes@users.noreply.github.com>
2023-07-25 13:52:58 -0500

    divisão para criação do modo infinito

--- alura.c
+++ alura.c
@@ -13,23 +13,49 @@ int main() {
 	char dnv;
 	do {
 			int nsecreto = random(); int tentativas = 1; int ganhou; int chute; int dificuldade; //declração de algumas variaveis
-		printf("em qual dificuldade voce deseja jogar?\n");
-		printf("1 - facil\n");
-		printf("2 - medio\n");
-		printf("3 - dificil\n");
+		//sistema de escolha de modo de jogo
+		printf("em qual modo deseja jogar?\n");
+		printf("0 - normal\n");
+		printf("1 - infinito\n");
+			int modo;
 			fflush(stdin);
-			scanf("%d", &dificuldade);
-		int ntentativas = qntvidas(dificuldade);
-		for(int i=0; i<ntentativas; i++) { 		//for que executa o jogo nas dificuldades 1 2 3
-			printf("tentativa %d de %d\n", tentativas, ntentativas);
-			printf("Qual o seu chute?");
-				fflush(stdin);
-				scanf("%d", &chute);
-			ganhou = attp(chute, nsecreto);
-			if(ganhou == 1) {
+			scanf("%d", &modo);
+		switch(modo) {
+			case 0: //modo normal
+				printf("em qual dificuldade voce deseja jogar?\n");
+				printf("1 - facil\n");
+				printf("2 - medio\n");
+				printf("3 - dificil\n");
+					fflush(stdin);
+					scanf("%d", &dificuldade);
+				int ntentativas = qntvidas(dificuldade);
+				for(int i=0; i<ntentativas; i++) { 		//for que executa o jogo nas dificuldades 1 2 3
+					printf("tentativa %d de %d\n", tentativas, ntentativas);
+					printf("Qual o seu chute? -> ");
+						fflush(stdin);
+						scanf("%d", &chute);
+					ganhou = attp(chute, nsecreto);
+					if(ganhou == 1) {
+					break;
+					}
+				}
 				break;
-			}
+			case 1: //modo infinito
+
+
+
+
+
+
+
+
+
+				break;
+			default:
+				printf("escolha um modo de jogo valido");
+				continue;
 		}
+
 	printf("\n\n");
 		vitoria(ganhou);
 	printf("\n\n");
