63127520b1bcae9ff6e42fc07b89af694f56de3b
Lucas Lopes <lkmrlucas@gmail.com>
2023-12-11 17:20:37 -0500

    ignorar comandos que nao movam o player / prox

--- corre/pacman.c
+++ corre/pacman.c
@@ -20,28 +20,41 @@ int main() {
 }
 
 void move(char dir) {
-    m.matriz[pers.y][pers.x] = '.';
+    if(dir!='w'&& dir!='a'&& dir!='s'&& dir!='d') {
+        return; //return em void = matar a função
+    }
+    
+    int prox_x = pers.x;
+    int prox_y = pers.y;
 
-    //achar a posição do personagem no mapa
-
-    switch(dir) {
+    switch(dir) { 
         case 'w':
-            m.matriz[pers.y-1][pers.x] = '@';
-            pers.y--;
+            prox_x--;
             break;
         case 'a':
-            m.matriz[pers.y][pers.x-1] = '@';
-            pers.x--;
+            prox_y--;
             break;
         case 's':
-            m.matriz[pers.y+1][pers.x] = '@';
-            pers.y++;
+            prox_x++;
             break;
         case 'd':
-            m.matriz[pers.y][pers.x+1] = '@';
-            pers.x++;
+            prox_y++;
             break;
     }
+
+    if(prox_x>=m.linhas) { //se o proximo movimento for maior que o numero de lihas
+        return;
+    }
+    if(prox_y>=m.colunas) { //se o proximo movimento for maior que o numero de colunas
+        return;
+    }
+    if(m.matriz[prox_x][prox_y] != '.') { //se o proximo movimento for para uma casa diferente de '.'
+        return; //mate a função
+    }
+        m.matriz[prox_x][prox_y] = '@'; //proxima casa vira o personagem
+        m.matriz[pers.x][pers.y] = '.'; //casa anterior vira um ponto
+        pers.x = prox_x;
+        pers.y = prox_y; //atualiza a posição do personagem para a proxima posição
 }
 
 int acabou() {
